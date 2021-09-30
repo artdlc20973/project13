75a0b70ed792c77827b7d455c32bf8c4792dc67c
Art <77998829+artdlc20973@users.noreply.github.com>
2021-09-30 16:08:47 -0600

    project13 in 50projects 50days

---
+++ script.js
@@ -0,0 +1,22 @@
+const tagesEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
+
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+})
+
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !=='').map(tag => tag.trim())
+
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        cont tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl) 
+    });
+
+}
